4f9de54d11f63577412a42c2be29b74fa99d0032
Sanyogita Borade <sanyogita.borade@gmail.com>
2020-12-10 20:16:53 +0530

    Mywebsite Added

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld
+ {
+      public static void main(String[] args) 
+      { 
+          System.out.println("Hello, World!");
+          System.out.println("Git + Jenskins!");
+          System.out.println("Hello Jenskins!");
+          System.out.println("Git Jenskins!");
+          System.out.println("In the Jenskins!");
+          System.out.println("Jenskins!");
+       }
+   }
